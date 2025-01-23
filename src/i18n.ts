225cd742d2a41092c76044a94356fb36a44722db
Felix Wei <felixwei7@gmail.com>
2025-01-23 10:25:49 -0800

    added eslint comments to disable compile warnings

--- src/i18n.ts
+++ src/i18n.ts
@@ -5,9 +5,11 @@ import {getRequestConfig} from 'next-intl/server';
 const locales = ['en', 'fr'];
  
 export default getRequestConfig(async ({ requestLocale }) => {
+  // eslint-disable-next-line prefer-const
   let locale = await requestLocale;
 
   // Validate that the incoming `locale` parameter is valid
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!locales.includes(locale as any)) notFound();
  
   return {
