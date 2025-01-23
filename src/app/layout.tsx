b538ec0dca03da173d988bea5b26fd2f876db2e1
Felix Wei <felixwei7@gmail.com>
2025-01-22 23:13:06 -0800

    language localization setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const favicon = "/icons/golf_club.svg";
-
-export const metadata: Metadata = {
-  title: "Golf Shop",
-  description: "The best place to buy golf equipment online",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang='en' suppressHydrationWarning>
-      <head>
-        <link rel="icon" href={favicon} type="image/svg+xml" /> 
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+  return children;
 }
