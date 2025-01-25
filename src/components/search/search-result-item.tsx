16ba5726a392a27115c8df49bf91fd1001c93682
Felix Wei <felixwei7@gmail.com>
2025-01-25 15:46:23 -0800

    css touchups

--- src/components/search/search-result-item.tsx
+++ src/components/search/search-result-item.tsx
@@ -11,58 +11,70 @@ interface SearchItemProps {
 const SearchResultItem: React.FC<SearchItemProps> = ({ item }) => {
   return (
     <Card
-      key={item.id}
-      sx={{
-        maxWidth: 345,
-        maxHeight: 400,
-        margin: 'auto',
-        borderRadius: 2,
-        boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
-      }}
-    >
-      <CardContent>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '100%', 
-            height: '100%',
-            overflow: 'hidden', 
-            borderTopLeftRadius: 8,
-            borderTopRightRadius: 8,
-          }}
+        key={item.id}
+        sx={{
+            maxWidth: 345,
+            maxHeight: 400,
+            margin: 'auto',
+            borderRadius: 2,
+            boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+        }}
         >
-          <CardMedia
-            component="img"
-            image={item.imageUrl}
-            alt={item.name}
+        <CardContent
             sx={{
-                width: 140, 
-                height: 140,
-                objectFit: 'cover', 
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start', 
+            padding: 2, 
+            height: '100%', 
             }}
-          />
-        </Box>
-        <Typography
-          variant="h6"
-          component="div"
-          sx={{
-            fontWeight: 'bold',
-            textAlign: 'center',
-            marginBottom: 1,
-          }}
         >
-          {item.name}
-        </Typography>
-        <Typography
-          variant="body1"
-          color="text.secondary"
-          sx={{ textAlign: 'center' }}
-        >
-          ${item.price.toFixed(2)}
-        </Typography>
-      </CardContent>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    width: '100%',
+                    height: 140, 
+                    overflow: 'hidden',
+                    borderTopLeftRadius: 8,
+                    borderTopRightRadius: 8,
+                }}
+            >
+                <CardMedia
+                    component="img"
+                    image={item.imageUrl}
+                    alt={item.name}
+                    sx={{
+                    width: 140,
+                    height: 140,
+                    objectFit: 'cover',
+                    borderRadius: '8px',
+                    }}
+                />
+            </Box>
+            
+            <Typography
+                variant="h6"
+                component="div"
+                sx={{
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    marginTop: 1, // Space between the image and text
+                    marginBottom: 1,
+                }}
+            >
+                {item.name}
+            </Typography>
+
+            <Typography
+                variant="body1"
+                color="text.secondary"
+                sx={{ textAlign: 'center' }}
+            >
+                ${item.price.toFixed(2)}
+            </Typography>
+
+        </CardContent>
     </Card>
   );
 };
