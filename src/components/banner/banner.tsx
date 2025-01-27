9fd7eb35e21539f38aa3252e7af8098a03434c5e
Felix Wei <felixwei7@gmail.com>
2025-01-26 22:50:11 -0800

    added close animation

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -4,28 +4,27 @@ import { useState } from 'react';
 
 export default function Banner() {
   const [isVisible, setIsVisible] = useState(true);
+  const [isClosing, setIsClosing] = useState(false); 
   const bannerURL = "./banner/rick_shiels_cut_slowed.gif";
 
   const handleClose = () => {
-    setIsVisible(false);
+    setIsClosing(true);
+    setTimeout(() => setIsVisible(false), 500); 
   };
-
-
 
   return (
     isVisible && (
       <>
-      <div className="banner-container">
-        <div className="banner">
-          <img src={bannerURL} alt="Banner Gif" className="banner-gif" />
-          <button className="close-btn" onClick={handleClose}>
-            X
-          </button>
+        <div className={`banner-container ${isClosing ? "slideUp" : ""}`}>
+          <div className="banner">
+            <img src={bannerURL} alt="Banner Gif" className="banner-gif" />
+            <button className="close-btn" onClick={handleClose}>
+              X
+            </button>
+          </div>
         </div>
-      </div>
 
-      <div className="banner-displacement">
-      </div>
+        <div className="banner-displacement"></div>
       </>
     )
   );
